3415644302d2469bd2310c8d3f24d8c81e36512d
Janaharajan <janaharajannd2000@gmail.com>
2025-05-14 13:22:40 +0530

    latest

---
+++ arrowfunction.ts
@@ -0,0 +1,28 @@
+let emp1: { employeename: string, employeeage: number, somebol: boolean } = {
+
+
+    employeename: "janaharajan",
+
+
+    employeeage: 25,
+
+
+    somebol: false,
+
+
+
+
+
+}
+
+
+let myfunc = () => {
+
+
+    console.log(emp1)
+
+
+}
+
+
+myfunc();
---
+++ type.ts
@@ -0,0 +1,25 @@
+
+
+
+let indianAllycountries: string[] = ["russia", "japan", "france", "italy"]
+
+
+
+
+
+let printfunc = (indianAllycountries: string[]) => {
+
+
+    for (let i: number = 0; i < indianAllycountries.length; i++) {
+
+
+        console.log(`${indianAllycountries[i]} is indian ally`)
+
+
+    }
+
+
+}
+
+
+printfunc(indianAllycountries);
