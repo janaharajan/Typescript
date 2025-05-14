365fdf4898f431eecc7e36e8550429e0c8943373
Janaharajan <janaharajannd2000@gmail.com>
2025-05-14 18:18:31 +0530

    adding swicth.ts to github

---
+++ switch.ts
@@ -0,0 +1,19 @@
+let flower: string[] = ["jasmine", "lily", "lotus", "marrygold", "rose", "hibbiscus"];
+let myselection = flower[1]
+switch (myselection) {
+    case "jasmine":
+        console.log(`flower you selected is ${myselection}`)
+        break;
+    case "lily":
+        console.log(`flower you selected is ${myselection}`)
+        break;
+    case "lotus":
+        console.log(`flower you selected is ${myselection}`)
+        break; case "marrygold":
+        console.log(`flower you selected is ${myselection}`)
+        break; case "rose":
+        console.log(`flower you selected is ${myselection}`)
+        break; case "hibbiscus":
+        console.log(`flower you selected is ${myselection}`)
+        break;
+}
